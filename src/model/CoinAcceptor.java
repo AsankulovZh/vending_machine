4a3a1d71c6f236ea57f3f4f53035964751445bc7
Zhyrgalbek <zhyrgalbek.myrzamatovich@gmail.com>
2024-05-05 20:25:08 +0800

    CoinAcceptor implements PaymentAcceptor

--- src/model/CoinAcceptor.java
+++ src/model/CoinAcceptor.java
@@ -1,17 +1,33 @@
 package model;
 
-public class CoinAcceptor {
+public class CoinAcceptor implements PaymentAcceptor {
     private int amount;
 
     public CoinAcceptor(int amount) {
         this.amount = amount;
     }
 
-    public int getAmount() {
-        return amount;
-    }
-
     public void setAmount(int amount) {
         this.amount = amount;
+    }
+
+    @Override
+    public boolean authentication() {
+        return false;
+    }
+
+    @Override
+    public int getAmount() {
+        return 0;
+    }
+
+    @Override
+    public void addAmount() {
+
+    }
+
+    @Override
+    public boolean deductAmount(int amount) {
+        return false;
     }
 }
